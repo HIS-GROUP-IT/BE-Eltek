0e06f05b82d6ea8054aaab5cdf5099a916d22f2e
KwanleNdaba <kwanele.ndaba@hisgroup-it.co.za>
2025-06-20 07:50:23 +0200

    feat(up)

--- src/middlewares/ErrorMiddleware.ts
+++ src/middlewares/ErrorMiddleware.ts
@@ -1,15 +1,35 @@
 import { NextFunction, Request, Response } from 'express';
 import { HttpException } from '../exceptions/HttpException';
 import { logger } from '@utils/logger';
+import { FRONT_END_URL } from '@/config';
 
-export const ErrorMiddleware = (error: HttpException, req: Request, res: Response, next: NextFunction) => {
+export const ErrorMiddleware = (
+  error: HttpException,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const status: number = error.status || 500;
     const message: string = error.message || 'Something went wrong';
 
-    logger.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`);
+    // 🔥 Log the error
+    logger.error(
+      `[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`
+    );
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin || FRONT_END_URL);
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, DELETE, OPTIONS'
+    );
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+
     res.status(status).json({ message });
-  } catch (error) {
-    next(error);
+  } catch (err) {
+    next(err);
   }
 };
